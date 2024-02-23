7680b4277938c6cb9610cbc280cac09085c20a3f
PandaOnCaffeine <Tuepvm@gmail.com>
2024-02-23 18:44:18 +0100

    Converter Example

--- Help Jacob SOLID/Classes/SMTPMessageSender.cs
+++ Help Jacob SOLID/Classes/SMTPMessageSender.cs
@@ -7,12 +7,24 @@ namespace Help_Jacob_SOLID.Classes
     /// </summary>
     public class SMTPMessageSender : IMessageSender
     {
+        private readonly HTMLConverter _converter;
+        public SMTPMessageSender(HTMLConverter converter)
+        {
+            _converter = converter;
+        }
+
         public void SendMessage(Message m)
         {
+            //Example of how to use converter
+            //_converter.Convert(m.Body);
+
             //her implementeres alt koden til at sende via Smtp
         }
         public void SendMessageToAll(string[] to, Message m)
         {
+            //Example of how to use converter
+            //_converter.Convert(m.Body);
+
             //her implementeres alt koden til at sende via Smtp
         }
     }
--- Help Jacob SOLID/Classes/VMessageSender.cs
+++ Help Jacob SOLID/Classes/VMessageSender.cs
@@ -7,12 +7,24 @@ namespace Help_Jacob_SOLID.Classes
     /// </summary>
     public class VMessageSender : IMessageSender
     {
-        public void SendMessage(Message message)
+        private readonly HTMLConverter _converter;
+        public VMessageSender(HTMLConverter converter)
         {
+            _converter = converter;
+        }
+
+        public void SendMessage(Message m)
+        {
+            //Example of how to use converter
+            //_converter.Convert(m.Body);
+
             //her implementeres alt koden til at sende via VMessage (Internal System)
         }
-        public void SendMessageToAll(string[] to, Message message)
+        public void SendMessageToAll(string[] to, Message m)
         {
+            //Example of how to use converter
+            //_converter.Convert(m.Body);
+
             //her implementeres alt koden til at sende via VMessage (Internal System)
         }
     }
